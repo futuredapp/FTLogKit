c4f4432d520474088c5cd4a5a8d8d8f4de456b1a
Michal Němec <michal.nemec@futured.app>
2022-10-10 20:01:56 +0300

    :tada: Init package

---
+++ Sources/FTLogKit/FTLogKit.swift
@@ -0,0 +1,6 @@
+public struct FTLogKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/FTLogKitTests/FTLogKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import FTLogKit
+
+final class FTLogKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(FTLogKit().text, "Hello, World!")
+    }
+}
